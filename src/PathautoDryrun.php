7b98bcc3e2b67d8044199d1be7a57d1df74df2a7
James Huang <jhu9000@gmail.com>
2020-04-19 18:53:00 -0700

    init

---
+++ src/PathautoDryrun.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Drupal\pathauto_dryrun;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Language\LanguageInterface;
+use Drupal\Core\Render\BubbleableMetadata;
+use Drupal\Core\Utility\Token;
+use Drupal\pathauto\AliasCleanerInterface;
+use Drupal\pathauto\PathautoGeneratorInterface;
+
+/**
+ * Provides methods for generating path aliases.
+ */
+class PathautoDryrun {
+
+  /**
+   * Config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * Token service.
+   *
+   * @var \Drupal\Core\Utility\Token
+   */
+  protected $token;
+
+  /**
+   * The alias cleaner.
+   *
+   * @var \Drupal\pathauto\AliasCleanerInterface
+   */
+  protected $aliasCleaner;
+
+  /**
+   * The alias cleaner.
+   *
+   * @var \Drupal\pathauto\PathautoGeneratorInterface
+   */
+  protected $pathautoGenerator;
+
+  /**
+   * Creates a new PathautoLite manager.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler.
+   * @param \Drupal\Core\Utility\Token $token
+   *   The token utility.
+   * @param \Drupal\pathauto\AliasCleanerInterface $alias_cleaner
+   *   The alias cleaner.
+   * @param \Drupal\pathauto\PathautoGeneratorInterface $pathauto_generator
+   *   The pathauto generator.
+   *
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, ModuleHandlerInterface $module_handler, Token $token, AliasCleanerInterface $alias_cleaner, PathautoGeneratorInterface $pathauto_generator) {
+    $this->configFactory = $config_factory;
+    $this->moduleHandler = $module_handler;
+    $this->token = $token;
+    $this->aliasCleaner = $alias_cleaner;
+    $this->pathautoGenerator = $pathauto_generator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getAlias(EntityInterface $entity) {
+    // Retrieve and apply the pattern for this content type.
+    $pattern = $this->pathautoGenerator->getPatternByEntity($entity);
+    if (empty($pattern)) {
+      // No pattern? Do nothing (otherwise we may blow away existing aliases...)
+      return NULL;
+    }
+
+    $config = $this->configFactory->get('pathauto.settings');
+    $langcode = $entity->language()->getId();
+
+    // Core does not handle aliases with language Not Applicable.
+    if ($langcode == LanguageInterface::LANGCODE_NOT_APPLICABLE) {
+      $langcode = LanguageInterface::LANGCODE_NOT_SPECIFIED;
+    }
+
+    // Build token data.
+    $data = [
+      $entity->getEntityTypeId() => $entity,
+    ];
+
+    // Allow other modules to alter the pattern.
+    $context = [
+      'module' => $entity->getEntityType()->getProvider(),
+      'data' => $data,
+      'bundle' => $entity->bundle(),
+      'language' => &$langcode,
+    ];
+    $pattern_original = $pattern->getPattern();
+    $this->moduleHandler->alter('pathauto_pattern', $pattern, $context);
+    $pattern_altered = $pattern->getPattern();
+
+    // Replace any tokens in the pattern.
+    // Uses callback option to clean replacements. No sanitization.
+    // Pass empty BubbleableMetadata object to explicitly ignore cacheablity,
+    // as the result is never rendered.
+    $alias = $this->token->replace($pattern->getPattern(), $data, [
+      'clear' => TRUE,
+      'callback' => [$this->aliasCleaner, 'cleanTokenValues'],
+      'langcode' => $langcode,
+      'pathauto_lite' => TRUE,
+    ], new BubbleableMetadata());
+
+    // Check if the token replacement has not actually replaced any values. If
+    // that is the case, then stop because we should not generate an alias.
+    // @see token_scan()
+    $pattern_tokens_removed = preg_replace('/\[[^\s\]:]*:[^\s\]]*\]/', '', $pattern->getPattern());
+    if ($alias === $pattern_tokens_removed) {
+      return NULL;
+    }
+
+    $alias = $this->aliasCleaner->cleanAlias($alias);
+
+    // Allow other modules to alter the alias.
+    $context['pattern'] = $pattern;
+    $this->moduleHandler->alter('pathauto_alias', $alias, $context);
+
+    // If we have arrived at an empty string, discontinue.
+    if (!mb_strlen($alias)) {
+      return NULL;
+    }
+
+    return $alias;
+  }
+}
